ecf49b224ef72f8540c3121e290eed8f3d88b5be
wutieshan <1766501249@qq.com>
2025-06-03 18:16:12 +0800

    tieshan

---
+++ devtools/autoclick.py
@@ -0,0 +1,23 @@
+"""
+鼠标自动点击
+
+可自由调整的点:
+    1. 点击的频率
+    2. 鼠标按键选择
+"""
+
+import pynput
+
+
+class Autoclick:
+    class MouseButton:
+        pass
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def _do_click(self):
+        pass
